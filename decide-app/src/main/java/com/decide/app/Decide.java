5f832363b325deee21f967ee5f535502b95c0e43
Alex Gunnarsson <alexgunnarsson@live.se>
2024-01-25 12:47:28 +0100

    feat: add placeholder for computing PUM and FUV

--- decide-app/src/main/java/com/decide/app/Decide.java
+++ decide-app/src/main/java/com/decide/app/Decide.java
@@ -16,7 +16,23 @@ public class Decide {
     }
 
     public boolean DECIDE() {
+        LIC lic = new LIC(NUMPOINTS, POINTS, PARAMETERS);
+        boolean[] CMV = lic.getCMV();
+        boolean[][] PUM = computePUM(CMV);
+        boolean[] FUV = computeFUV(PUM);
+        for (boolean value : FUV) {
+            if (!value) return false;
+        }
         return true;
     }
 
+    public boolean[][] computePUM(boolean[] CMV) {
+        boolean[][] PUM = new boolean[][]{{ true }};
+        return PUM;
+    }
+
+    public boolean[] computeFUV(boolean[][] PUM) {
+        boolean[] FUV = new boolean[]{ true };
+        return FUV;
+    }
 }
