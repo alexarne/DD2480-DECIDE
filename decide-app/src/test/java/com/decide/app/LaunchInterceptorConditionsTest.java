3e7b084b65fd9b85be257a39f1b0ad457d9db249
haaker1 <haaker.anne@gmail.com>
2024-01-26 11:22:30 +0100

    Revert "test: add 4 tests for LIC0"
    This reverts commit 93c99bd364a4ead9e26df048a203a017628cf157.

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -2,11 +2,7 @@ package com.decide.app;
 
 import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.assertFalse;
-
 import org.junit.Test;
-
-import org.junit.Ignore;
 
 public class LaunchInterceptorConditionsTest {
 
@@ -18,46 +14,6 @@ public class LaunchInterceptorConditionsTest {
         Parameters PARAMETERS = new Parameters();
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertTrue(LIC.getLaunchInterceptorCondition0());
-    }
-
-    @Ignore
-    @Test
-    public void LIC0TrueDistanceGreaterThanLENGTH1(){
-        int NUMPOINTS = 2;
-        Parameters PARAMETERS = new Parameters();
-        Point[] POINTS = new Point[]{ new Point(1, 2) , new Point(1, 2+PARAMETERS.LENGTH1+1)};
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        assertTrue(LIC.getLaunchInterceptorCondition0());
-    }
-
-    @Ignore
-    @Test
-    public void LIC0FalseDistanceSmallerThanLENGTH1(){
-        int NUMPOINTS = 2;
-        Parameters PARAMETERS = new Parameters();
-        Point[] POINTS = new Point[]{ new Point(1, 2) , new Point(1, 2+PARAMETERS.LENGTH1-1)};
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        assertFalse(LIC.getLaunchInterceptorCondition0());
-    }
-
-    @Ignore
-    @Test
-    public void LIC0FalseOnlyOnePoint(){
-        int NUMPOINTS = 1;
-        Parameters PARAMETERS = new Parameters();
-        Point[] POINTS = new Point[]{ new Point(1, 2)};
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        assertFalse(LIC.getLaunchInterceptorCondition0());
-    }
-
-    @Ignore
-    @Test
-    public void LIC0FalseDistanceEqualToLENGTH1(){
-        int NUMPOINTS = 1;
-        Parameters PARAMETERS = new Parameters();
-        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(1, 2+PARAMETERS.LENGTH1)};
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        assertFalse(LIC.getLaunchInterceptorCondition0());
     }
 
 }
