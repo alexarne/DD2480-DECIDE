181e60ff10e16bad83ae9199a4eab0bc32c48247
TRICOT-Hugo <tricot@kth.se>
2024-01-26 12:03:03 +0100

    rebase: rebase test cases for LIC0 in the LIC1 test development branch

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -1,11 +1,9 @@
 package com.decide.app;
 
 import static org.junit.Assert.assertTrue;
-
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
-
 import org.junit.Ignore;
 
 public class LaunchInterceptorConditionsTest {
@@ -30,6 +28,7 @@ public class LaunchInterceptorConditionsTest {
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertTrue(LIC.getLaunchInterceptorCondition0());
     }
+    
 
     @Ignore
     @Test
@@ -41,6 +40,7 @@ public class LaunchInterceptorConditionsTest {
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
     }
+    
 
     @Ignore
     @Test
@@ -52,6 +52,7 @@ public class LaunchInterceptorConditionsTest {
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
     }
+    
 
     @Ignore
     @Test
@@ -62,6 +63,87 @@ public class LaunchInterceptorConditionsTest {
         Point[] POINTS = new Point[]{ new Point(1, 2), new Point(1, 2+PARAMETERS.LENGTH1)};
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
+    }
+
+
+    @Ignore
+    @Test
+    public void positiveLIC1Test() {
+        // Setup
+        Parameters PARAMETERS = new Parameters();
+        PARAMETERS.RADIUS1 = 4.0;
+        int NUMPOINTS = 3;
+        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(3, 4), new Point(4, 4) };
+        
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        
+        // Assertion
+        assertTrue(LIC.getLaunchInterceptorCondition1());
+    }
+
+    @Ignore
+    @Test
+    public void negativeLIC1Test() {
+        // Setup
+        Parameters PARAMETERS = new Parameters();
+        PARAMETERS.RADIUS1 = 2.0;
+        int NUMPOINTS = 4;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(5, 2.5), new Point(3, 5), new Point(0, 0) };
+        
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        
+        // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition1());
+    }
+
+    @Ignore
+    @Test
+    public void edgeCasePointsOnCircleExtremitiesLIC1Test() {
+        // Setup
+        Parameters PARAMETERS = new Parameters();
+        PARAMETERS.RADIUS1 = 3.0;
+        int NUMPOINTS = 3;
+        Point[] POINTS = new Point[]{ new Point(4, 4), new Point(4, 1), new Point(1, 4) };
+        
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        
+        // Assertion
+        assertTrue(LIC.getLaunchInterceptorCondition1());
+    }
+
+    @Ignore
+    @Test
+    public void incoherentInputLIC1Test() {
+        // Setup
+        Parameters PARAMETERS = new Parameters();
+        PARAMETERS.RADIUS1 = 4.0;
+        int NUMPOINTS = 3;
+        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(3, 4) };
+        
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        
+        // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition1());  // May be changed for an Exception raised
+    }
+
+    @Ignore
+    @Test
+    public void unsufficientInputLIC1Test() {
+        // Setup
+        Parameters PARAMETERS = new Parameters();
+        PARAMETERS.RADIUS1 = 10.0;
+        int NUMPOINTS = 2;
+        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(3, 4) };
+        
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        
+        // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition1());
     }
 
 }
