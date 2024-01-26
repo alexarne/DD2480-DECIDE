87a257e859ef9a359e2d44f2eaa63969b81f6b6e
haaker1 <haaker.anne@gmail.com>
2024-01-26 11:52:07 +0100

    refactor: include value for LENGTH1 in tests for LIC0

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -25,6 +25,7 @@ public class LaunchInterceptorConditionsTest {
     public void LIC0TrueDistanceGreaterThanLENGTH1(){
         int NUMPOINTS = 2;
         Parameters PARAMETERS = new Parameters();
+        PARAMETERS.LENGTH1 = 4;
         Point[] POINTS = new Point[]{ new Point(1, 2) , new Point(1, 2+PARAMETERS.LENGTH1+1)};
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertTrue(LIC.getLaunchInterceptorCondition0());
@@ -35,6 +36,7 @@ public class LaunchInterceptorConditionsTest {
     public void LIC0FalseDistanceSmallerThanLENGTH1(){
         int NUMPOINTS = 2;
         Parameters PARAMETERS = new Parameters();
+        PARAMETERS.LENGTH1 = 4;
         Point[] POINTS = new Point[]{ new Point(1, 2) , new Point(1, 2+PARAMETERS.LENGTH1-1)};
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
@@ -45,6 +47,7 @@ public class LaunchInterceptorConditionsTest {
     public void LIC0FalseOnlyOnePoint(){
         int NUMPOINTS = 1;
         Parameters PARAMETERS = new Parameters();
+        PARAMETERS.LENGTH1 = 2;
         Point[] POINTS = new Point[]{ new Point(1, 2)};
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
@@ -55,6 +58,7 @@ public class LaunchInterceptorConditionsTest {
     public void LIC0FalseDistanceEqualToLENGTH1(){
         int NUMPOINTS = 1;
         Parameters PARAMETERS = new Parameters();
+        PARAMETERS.LENGTH1 = 2;
         Point[] POINTS = new Point[]{ new Point(1, 2), new Point(1, 2+PARAMETERS.LENGTH1)};
         LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
         assertFalse(LIC.getLaunchInterceptorCondition0());
