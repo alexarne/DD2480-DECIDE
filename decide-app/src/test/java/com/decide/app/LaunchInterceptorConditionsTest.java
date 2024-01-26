515899bdbe5fec0e300e798cc9840cea9755bbb8
Alex Gunnarsson <alexgunnarsson@live.se>
2024-01-26 22:45:03 +0100

    refactor: remove base test using LIC0

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -10,16 +10,6 @@ import org.junit.Ignore;
 
 public class LaunchInterceptorConditionsTest {
 
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        int NUMPOINTS = 1;
-        Point[] POINTS = new Point[]{ new Point(1, 2) };
-        Parameters PARAMETERS = new Parameters();
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        assertTrue(LIC.getLaunchInterceptorCondition0());
-    }
-
     /**
      * ========================== [ LIC 0 ] ==========================
      */
