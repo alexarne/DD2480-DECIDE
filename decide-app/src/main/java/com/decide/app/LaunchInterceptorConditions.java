5145ec1d16c308524d498484b48e4c0d209483aa
haaker1 <haaker.anne@gmail.com>
2024-01-29 10:36:32 +0100

    refactor: put conditionals checking if points coincide with vertex on same line.

--- decide-app/src/main/java/com/decide/app/LaunchInterceptorConditions.java
+++ decide-app/src/main/java/com/decide/app/LaunchInterceptorConditions.java
@@ -65,8 +65,9 @@ public class LaunchInterceptorConditions {
         double y2;
         for(int i = 0; i < NUMPOINTS-2; i++){
             vertex = POINTS[i+1];
-            if(POINTS[i].getX() == vertex.getX() && POINTS[i].getY() == vertex.getY()) return false;
-            else if(POINTS[i+2].getX() == vertex.getX() && POINTS[i+2].getY() == vertex.getY()) return false;
+            if(POINTS[i].getX() == vertex.getX() && POINTS[i].getY() == vertex.getY()
+            || POINTS[i+2].getX() == vertex.getX() && POINTS[i+2].getY() == vertex.getY()) return false;
+            //else if(POINTS[i+2].getX() == vertex.getX() && POINTS[i+2].getY() == vertex.getY()) return false;
             x1 = (POINTS[i].getX() - vertex.getX());
             y1 = (POINTS[i].getY() - vertex.getY());
             x2 = (POINTS[i+2].getX() - vertex.getX());
