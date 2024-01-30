d4f2935b46acb68237a2b63d300519a7ae3ede41
Juan Lavagnini <juanlavagnini@gmail.com>
2024-01-30 11:16:33 +0100

    fix: need this changes to make my LIC work
    Probably this changes are only circumstantial because at least for this moment i dont have the final set of points

--- decide-app/src/test/java/com/decide/app/DecideTest.java
+++ decide-app/src/test/java/com/decide/app/DecideTest.java
@@ -9,10 +9,12 @@ public class DecideTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        int NUMPOINTS = 1;
-        Point[] POINTS = new Point[]{ new Point(1, 2) };
+        int NUMPOINTS = 3;
+        Point[] POINTS = new Point[]{ new Point(1, 2),new Point(-1, 2),new Point(1, -2) };
         Parameters PARAMETERS = new Parameters();
         PARAMETERS.AREA1 = 1.0;
+        PARAMETERS.QUADS = 1;
+        PARAMETERS.Q_PTS = 3;
         Connector[][] LCM = new Connector[][]{{ Connector.ANDD }};
         boolean[] PUV = new boolean[]{ true };
         Decide decide = new Decide(NUMPOINTS, POINTS, PARAMETERS, LCM, PUV);
