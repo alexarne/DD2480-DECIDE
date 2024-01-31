2856bfe746f5dbaef9aa5f76acd9c5708119a9fd
Alex Gunnarsson <75060613+alexarne@users.noreply.github.com>
2024-01-31 15:23:52 +0100

    Implement FUV + tests (#66)
    * feat: implement get FUV
    
    * feat: implement tests for FUV

--- decide-app/src/test/java/com/decide/app/DecideTest.java
+++ decide-app/src/test/java/com/decide/app/DecideTest.java
@@ -149,7 +149,89 @@ public class DecideTest {
      * =========================== [ FUV ] ===========================
      */
 
-    
+    /**
+     * Positive test case, ensure that the elements in the FUV are true 
+     * only if all elements in the corresponding row in the PUM are true, 
+     * or if the corresponding value in the PUV is false.
+     */
+    @Test
+    public void FUVCorrespondsToPUMandPUV() {
+        boolean[] PUV = new boolean[]{
+            true,
+            true,
+            true,
+            true,
+            false,
+            true,
+            false,
+            true,
+            false,
+            true,
+            true,
+            true,
+            true,
+            false,
+            true
+        };
+        Decide decide = new Decide(0, null, PARAMETERS, null, PUV);
+        boolean[][] PUM = new boolean[][]{
+            {true, true, true, true, true, true, true, true, false, true, true, true, true, true, true},
+            {true, true, true, true, false, true, false, true, false, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true, true, true, true, false, true},
+            {true, true, true, true, false, true, true, true, false, true, true, true, true, false, true},
+            {true, false, true, false, false, true, true, true, false, true, true, false, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
+            {true, false, true, true, true, true, false, true, false, false, true, true, true, false, true},
+            {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true},
+            {false, false, true, false, false, true, false, true, true, true, false, true, false, false, true},
+            {true, true, true, true, true, true, false, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, false, true, true, true, true, false, true},
+            {true, true, true, true, false, true, true, true, true, true, true, true, true, false, true},
+            {true, true, true, true, true, true, true, true, false, true, true, true, true, false, true},
+            {true, true, false, false, true, true, false, true, false, true, false, false, false, true, true},
+            {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}
+        };
+        boolean[] FUV_CORRECT = new boolean[]{
+            false,
+            false,
+            false,
+            false,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+            true,
+            true
+        };
+        boolean[] FUV = decide.getFinalUnlockingVector(PUM);
+        assertTrue(FUV_CORRECT.length == FUV.length);
+        for (int i = 0; i < FUV.length; ++i) {
+            assertTrue(FUV[i] == FUV_CORRECT[i]);
+        }
+    }
+
+    /**
+     * Invalid input test case, ensure that an IllegalArgumentException
+     * is thrown if the input dimensions are not correct.
+     */
+    @Test
+    public void FUVThrowsExceptionOnInvalidDimensions() {
+        boolean[] PUV = new boolean[]{ true, true, true };
+        Decide decide = new Decide(0, null, PARAMETERS, null, PUV);
+        boolean[][] PUM = new boolean[][]{
+            {true, true, true},
+            {true, true, true}
+        };
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> { decide.getFinalUnlockingVector(PUM); }
+        );
+    }
 
     /**
      * ========================= [ LAUNCH ] ==========================
