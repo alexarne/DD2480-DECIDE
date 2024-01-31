daa02dda19b4ef5555039c6d151161ef0b58c1fd
TRICOT-Hugo <tricot@kth.se>
2024-01-31 15:22:10 +0100

    test: invalid argument test case for LIC10 - negative or null E_PTS

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -483,6 +483,34 @@ public class LaunchInterceptorConditionsTest {
         );
     }
 
+    /**
+     * Illegal argument value test case for LIC10, ensures an exception is raised if the
+     * value of E_PTS is negative or null.
+     */
+    @Ignore
+    @Test
+    public void LIC10InvalidArgumentNegativeOrNullEpts() {
+        PARAMETERS.E_PTS = 0;
+        PARAMETERS.AREA1 = 1;
+        Point[] POINTS = new Point[]{
+            new Point(0, 2),
+            new Point(1, 2),
+            new Point(2, 5),
+            new Point(0, 0),
+            new Point(1, 10),
+            new Point(1, 2),
+            new Point(2, 0)
+        };
+        int NUMPOINTS = POINTS.length;
+
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> { LIC.getLaunchInterceptorCondition10(); }
+        );
+    }
+
 
     
     /**
