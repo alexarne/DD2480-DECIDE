8ecb338a9f46311458a9f29e8f2d737930bde2d4
haaker1 <haaker.anne@gmail.com>
2024-01-31 16:21:03 +0100

    refactor: move LIC8 tests to correct place in test-file.

--- decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
+++ decide-app/src/test/java/com/decide/app/LaunchInterceptorConditionsTest.java
@@ -539,6 +539,135 @@ public class LaunchInterceptorConditionsTest {
      * ========================== [ LIC 8 ] ==========================
      */
 
+    /**
+     * Positive test case. Ensure LIC8 is satisfied when three points separated by 
+     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
+     * cannot be contained within or on a circle of radius RADIUS1.
+     */
+    @Test 
+    public void LIC8TruePointsNotInRadius(){
+        PARAMETERS.RADIUS1 = 2.0;
+        PARAMETERS.A_PTS = 1;
+        PARAMETERS.B_PTS = 2;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
+        int NUMPOINTS = POINTS.length;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertTrue(LIC.getLaunchInterceptorCondition8());
+
+    }
+
+    /**
+     * Negative test case. Ensure LIC8 is not satisfied when three points separated by 
+     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
+     * can be contained within or on a circle of radius RADIUS1.
+     */
+    @Test 
+    public void LIC8FalsePointsInRadius(){
+        PARAMETERS.RADIUS1 = 4.0;
+        PARAMETERS.A_PTS = 1;
+        PARAMETERS.B_PTS = 2;
+        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(0, 0), new Point(1, 2.5), new Point(1, 2), new Point(3, 3), new Point(1, 3), new Point(0, 0)  };
+        int NUMPOINTS = POINTS.length;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition8());
+    }
+
+    /**
+     * Edge case test case. Ensure LIC8 is not satisfied when three points separated by 
+     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
+     * are on the edge of a circle of radius RADIUS1.
+     */
+    @Test 
+    public void LIC8FalsePointsOnRadiusEdge(){
+        PARAMETERS.RADIUS1 = 3.0;
+        PARAMETERS.A_PTS = 1;
+        PARAMETERS.B_PTS = 1;
+        Point[] POINTS = new Point[]{ new Point(7, 4), new Point(10, 10), new Point(4, 1), new Point(0, 7), new Point(1, 4) };
+        int NUMPOINTS = POINTS.length;
+          // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+         // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition8());
+        
+    }
+
+    /**
+     * Incorrect input test case. Ensure LIC8 is not satisfied when less than 5 points
+     * are supplied.
+     */
+    @Test 
+    public void LIC8FalseLessThan5Points(){
+        PARAMETERS.RADIUS1 = 2.0;
+        PARAMETERS.A_PTS = 1;
+        PARAMETERS.B_PTS = 1;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(4, 2), new Point(1, 2), new Point(3, 5)};
+        int NUMPOINTS = POINTS.length;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertFalse(LIC.getLaunchInterceptorCondition8());
+
+    }
+
+    /**
+     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
+     * A_PTS is less than 1.
+     */
+    @Test 
+    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidA_PTS(){
+        PARAMETERS.A_PTS = 0;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
+        int NUMPOINTS = POINTS.length;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> { LIC.getLaunchInterceptorCondition8(); }
+        );
+
+    }
+
+    /**
+     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
+     * B_PTS is less than 1.
+     */
+    @Test 
+    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidB_PTS(){
+        PARAMETERS.B_PTS = -2;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
+        int NUMPOINTS = POINTS.length;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> { LIC.getLaunchInterceptorCondition8(); }
+        );
+    }
+
+    /**
+     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
+     * A_PTS + B_PTS > NUMPOINTS-3.
+     */
+    @Test 
+    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidNUMPOINTS(){
+        PARAMETERS.A_PTS = 1;
+        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
+        int NUMPOINTS = POINTS.length;
+        PARAMETERS.B_PTS = NUMPOINTS-2-PARAMETERS.A_PTS;
+        // Processing
+        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
+        // Assertion
+        assertThrows(
+            IllegalArgumentException.class, 
+            () -> { LIC.getLaunchInterceptorCondition8(); }
+        );
+    }
     
 
     /**
@@ -779,147 +908,7 @@ public class LaunchInterceptorConditionsTest {
      * ========================= [ LIC 14 ] ==========================
      */
     
-  
-  
-  
-  
 
-    
-    
-    
-    /**
-     * ========================== [ LIC 8 ] ==========================
-     */
-
-    /**
-     * Positive test case. Ensure LIC8 is satisfied when three points separated by 
-     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
-     * cannot be contained within or on a circle of radius RADIUS1.
-     */
-    @Test 
-    public void LIC8TruePointsNotInRadius(){
-        PARAMETERS.RADIUS1 = 2.0;
-        PARAMETERS.A_PTS = 1;
-        PARAMETERS.B_PTS = 2;
-        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
-        int NUMPOINTS = POINTS.length;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertTrue(LIC.getLaunchInterceptorCondition8());
-
-    }
-
-    /**
-     * Negative test case. Ensure LIC8 is not satisfied when three points separated by 
-     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
-     * can be contained within or on a circle of radius RADIUS1.
-     */
-    @Test 
-    public void LIC8FalsePointsInRadius(){
-        PARAMETERS.RADIUS1 = 4.0;
-        PARAMETERS.A_PTS = 1;
-        PARAMETERS.B_PTS = 2;
-        Point[] POINTS = new Point[]{ new Point(1, 2), new Point(0, 0), new Point(1, 2.5), new Point(1, 2), new Point(3, 3), new Point(1, 3), new Point(0, 0)  };
-        int NUMPOINTS = POINTS.length;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertFalse(LIC.getLaunchInterceptorCondition8());
-    }
-
-    /**
-     * Edge case test case. Ensure LIC8 is not satisfied when three points separated by 
-     * exactly A_PTS and B_PTS consecutive intervening points, respectively, 
-     * are on the edge of a circle of radius RADIUS1.
-     */
-    @Test 
-    public void LIC8FalsePointsOnRadiusEdge(){
-        PARAMETERS.RADIUS1 = 3.0;
-        PARAMETERS.A_PTS = 1;
-        PARAMETERS.B_PTS = 1;
-        Point[] POINTS = new Point[]{ new Point(7, 4), new Point(10, 10), new Point(4, 1), new Point(0, 7), new Point(1, 4) };
-        int NUMPOINTS = POINTS.length;
-          // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-         // Assertion
-        assertFalse(LIC.getLaunchInterceptorCondition8());
-        
-    }
-
-    /**
-     * Incorrect input test case. Ensure LIC8 is not satisfied when less than 5 points
-     * are supplied.
-     */
-    @Test 
-    public void LIC8FalseLessThan5Points(){
-        PARAMETERS.RADIUS1 = 2.0;
-        PARAMETERS.A_PTS = 1;
-        PARAMETERS.B_PTS = 1;
-        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(4, 2), new Point(1, 2), new Point(3, 5)};
-        int NUMPOINTS = POINTS.length;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertFalse(LIC.getLaunchInterceptorCondition8());
-
-    }
-
-    /**
-     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
-     * A_PTS is less than 1.
-     */
-    @Test 
-    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidA_PTS(){
-        PARAMETERS.A_PTS = 0;
-        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
-        int NUMPOINTS = POINTS.length;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertThrows(
-            IllegalArgumentException.class, 
-            () -> { LIC.getLaunchInterceptorCondition8(); }
-        );
-
-    }
-
-    /**
-     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
-     * B_PTS is less than 1.
-     */
-    @Test 
-    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidB_PTS(){
-        PARAMETERS.B_PTS = -2;
-        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
-        int NUMPOINTS = POINTS.length;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertThrows(
-            IllegalArgumentException.class, 
-            () -> { LIC.getLaunchInterceptorCondition8(); }
-        );
-    }
-
-    /**
-     * Invalid input test case. Ensure LIC8 throws IllegalArgumentException when
-     * A_PTS + B_PTS > NUMPOINTS-3.
-     */
-    @Test 
-    public void LIC8ThrowsIllegalArgumentExceptionOnInvalidNUMPOINTS(){
-        PARAMETERS.A_PTS = 1;
-        Point[] POINTS = new Point[]{ new Point(6, 1), new Point(0, 0), new Point(5, 2.5), new Point(1, 2), new Point(3, 3), new Point(3, 5), new Point(0, 0) };
-        int NUMPOINTS = POINTS.length;
-        PARAMETERS.B_PTS = NUMPOINTS-2-PARAMETERS.A_PTS;
-        // Processing
-        LaunchInterceptorConditions LIC = new LaunchInterceptorConditions(NUMPOINTS, POINTS, PARAMETERS);
-        // Assertion
-        assertThrows(
-            IllegalArgumentException.class, 
-            () -> { LIC.getLaunchInterceptorCondition8(); }
-        );
-    }
     
     /**
      * ========================= [ HELPERS ] =========================
