fdf98ee7ce80c1c9f3f592e01a826087c69681c1
haaker1 <haaker.anne@gmail.com>
2024-01-31 10:49:31 +0100

    refactor: remove unnecessary ccheck for point equality.

--- decide-app/src/main/java/com/decide/app/LaunchInterceptorConditions.java
+++ decide-app/src/main/java/com/decide/app/LaunchInterceptorConditions.java
@@ -143,10 +143,6 @@ public class LaunchInterceptorConditions {
         double x3 = p3.getX();
         double y3 = p3.getY();
 
-        // Points are on a vertical or horizontal line. Cannot form a circle
-        if((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)){
-            return Double.NaN;
-        }
         //two lines A through p1-p2 and B through p2-p3
         double slopeA = (y2-y1)/(x2-x1);
         double slopeB = (y3-y2)/(x3-x2);
